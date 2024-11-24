4814a3acc7ecbf6cbd627e1509eb0ae5429a9461
hemgom <reborn.april23@gmail.com>
2024-11-24 18:39:35 +0900

    [#2] feat: OrderController 수정

--- src/main/java/com/sparta/able/controller/OrderController.java
+++ src/main/java/com/sparta/able/controller/OrderController.java
@@ -1,17 +1,22 @@
 package com.sparta.able.controller;
 
+import com.sparta.able.dto.ResponseBodyDto;
 import com.sparta.able.dto.order.req.AddOrderReqDto;
-import com.sparta.able.dto.order.res.AddOrderResDto;
+import com.sparta.able.service.OrderService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public AddOrderResDto addOrder(@RequestBody @Valid AddOrderReqDto request) {
-        return null;
+    public ResponseBodyDto addOrder(@RequestBody @Valid AddOrderReqDto request) {
+        return orderService.addOrder(request);
     }
 }
