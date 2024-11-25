0ba69cbdc2c5017fca7dc082a33a09c5e5cf09ec
hemgom <reborn.april23@gmail.com>
2024-11-25 12:22:57 +0900

    [#2] feat: 응답방식을 컨벤션을 따르도록 수정

--- src/main/java/com/sparta/able/controller/OrderController.java
+++ src/main/java/com/sparta/able/controller/OrderController.java
@@ -1,12 +1,19 @@
 package com.sparta.able.controller;
 
-import com.sparta.able.dto.ResponseBodyDto;
 import com.sparta.able.dto.order.req.AddOrderReqDto;
+import com.sparta.able.dto.order.res.AddOrderResDto;
 import com.sparta.able.service.OrderService;
+import com.sparta.able.util.ResponseBodyDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.sparta.able.enums.ResponseMessage.ORDER_COMPLETED;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -15,8 +22,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseBodyDto addOrder(@RequestBody @Valid AddOrderReqDto request) {
-        return orderService.addOrder(request);
+    public ResponseEntity<ResponseBodyDto<AddOrderResDto>> addOrder(@RequestBody @Valid AddOrderReqDto request) {
+        AddOrderResDto data =  orderService.addOrder(request);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseBodyDto.success(ORDER_COMPLETED.getMessage(), data));
     }
 }
--- src/main/java/com/sparta/able/service/OrderService.java
+++ src/main/java/com/sparta/able/service/OrderService.java
@@ -1,22 +1,16 @@
 package com.sparta.able.service;
 
-import com.sparta.able.dto.ResponseBodyDto;
 import com.sparta.able.dto.order.req.AddOrderReqDto;
 import com.sparta.able.dto.order.res.AddOrderResDto;
 import com.sparta.able.entity.Order;
 import com.sparta.able.entity.Product;
 import com.sparta.able.entity.User;
-import com.sparta.able.enums.ResponseMessage;
-import com.sparta.able.enums.ResponseStatusCode;
 import com.sparta.able.repository.OrderRepository;
 import com.sparta.able.repository.ProductRepository;
 import com.sparta.able.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static com.sparta.able.enums.ResponseMessage.*;
-import static com.sparta.able.enums.ResponseStatusCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +20,7 @@ public class OrderService {
     private final ProductRepository productRepository;
 
     @Transactional
-    public ResponseBodyDto addOrder(AddOrderReqDto request) {
+    public AddOrderResDto addOrder(AddOrderReqDto request) {
         /*
         현재 인증/인가 에 대한 부분이 구현되지 않았으므로 임의적으로 RequestBody 에 요청 사용자 ID 를 전달 중
         인증/인가 를 반영하면 해당 부분 수정 필요
@@ -40,7 +34,6 @@ public class OrderService {
         Order newOrder = Order.create(customer, purchaseProduct.getId());
         orderRepository.save(newOrder);
 
-        AddOrderResDto addOrderResDto = AddOrderResDto.make(purchaseProduct.getName(), purchaseProduct.getPrice());
-        return ResponseBodyDto.make(SUCCESS.getCode(), ORDER_COMPLETED.getMessage(), addOrderResDto);
+        return AddOrderResDto.make(purchaseProduct.getName(), purchaseProduct.getPrice());
     }
 }
