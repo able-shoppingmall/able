cd7e21e7bc95b7954332b672a0bb392757984494
yeonwoochocho <cyw614c@gmail.com>
2024-11-25 12:06:36 +0900

    response 통일

--- src/main/java/com/sparta/able/controller/CouponController.java
+++ src/main/java/com/sparta/able/controller/CouponController.java
@@ -4,7 +4,9 @@ import com.sparta.able.dto.coupon.req.CouponRequestDto;
 import com.sparta.able.dto.coupon.res.CouponResponseDto;
 
 import com.sparta.able.service.CouponService;
+import com.sparta.able.util.ResponseBodyDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +18,18 @@ public class CouponController {
     private final CouponService couponService;
 
     @PostMapping
-    public ResponseEntity<CouponResponseDto> createCoupon(@RequestBody CouponRequestDto requestDto) {
-        CouponResponseDto responseDto = couponService.createCoupon(requestDto);
-        return ResponseEntity.ok(responseDto);
+    public ResponseEntity<ResponseBodyDto<CouponResponseDto>> createCoupon(@RequestBody CouponRequestDto requestDto) {
+        return new ResponseEntity<>(
+                ResponseBodyDto.success("사장님 쿠폰 생성 완료", couponService.createCoupon(requestDto)),
+                HttpStatus.OK);
     }
 
     @PostMapping("/event/{id}")
     public ResponseEntity<?> issueEventCoupon(@PathVariable Long id) {
-        String responseMessage = couponService.issueEventCoupon(id);
-        return ResponseEntity.ok().body(responseMessage);
+
+        return new ResponseEntity<>(
+                ResponseBodyDto.success("선착순 쿠폰 발급 완료", couponService.issueEventCoupon(id)),
+                HttpStatus.OK);
+
     }
 }
