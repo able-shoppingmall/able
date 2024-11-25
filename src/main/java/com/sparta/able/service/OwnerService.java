9f7b5fb9c9e6105d40f4b4b62d287e697b255a4e
kimjihye <hamiwood@naver.com>
2024-11-25 20:57:28 +0900

    refactor : 회원 코드 수정 + UserController api 수정 + OwnerService api 수정 + dummy 삭제

--- src/main/java/com/sparta/able/service/OwnerService.java
+++ src/main/java/com/sparta/able/service/OwnerService.java
@@ -38,11 +38,11 @@ public class OwnerService {
     public OwnerResponseDto LoginOwner(OwnerLoginRequestDto ownerLoginRequestDto) {
 
         Owner owner = ownerRepository.findByEmail(ownerLoginRequestDto.getEmail()).orElseThrow(
-                ()-> new InvalidRequestException("이메일 또는 비밀번호가 잘못되었습니다")
+                ()-> new ApplicationException(ErrorCode.INCORRECT_FORMAT)
         );
 
         if(!passwordEncoder.matches(ownerLoginRequestDto.getPassword(), owner.getPassword())) {
-            throw new InvalidRequestException("이메일 또는 비밀번호가 잘못되었습니다.");
+            throw new ApplicationException(ErrorCode.INCORRECT_FORMAT);
         }
 
         String token = jwtUtil.createToken(owner.getId(), owner.getEmail(), owner.getName(), "ROLE_OWNER");
