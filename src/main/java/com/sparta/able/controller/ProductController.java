231fe11246bea0fde09e5578e4bef392b031d191
Hamiwood <114393090+Hamiwood@users.noreply.github.com>
2024-11-26 11:07:24 +0900

    Update ProductController.java

--- src/main/java/com/sparta/able/controller/ProductController.java
+++ src/main/java/com/sparta/able/controller/ProductController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import com.sparta.able.dto.product.res.SearchResultDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
