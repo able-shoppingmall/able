105de07d508081382543afe15bd38954719252a3
hemgom <reborn.april23@gmail.com>
2024-11-26 15:06:15 +0900

    [#32] feat: KeywordRepository.findKeywordRank() 추가

--- src/main/java/com/sparta/able/repository/KeywordRepository.java
+++ src/main/java/com/sparta/able/repository/KeywordRepository.java
@@ -1,7 +1,14 @@
 package com.sparta.able.repository;
 
+import com.sparta.able.dto.keyword.res.KeywordRankDto;
 import com.sparta.able.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    @Query(value = "WITH keyword_rank AS (SELECT keyword, RANK() OVER(ORDER BY used_count) AS rank FROM keywords) " +
+            "SELECT * FROM keyword_rank LIMIT 10", nativeQuery = true)
+    List<KeywordRankDto> findKeywordRank();
 }
