07141b4c0e1d7817897c0b20c026e775ee37548b
hemgom <reborn.april23@gmail.com>
2024-11-26 15:08:22 +0900

    [#32] feat: KeywordController.findKeywordRank() 추가

--- src/main/java/com/sparta/able/controller/KeywordController.java
+++ src/main/java/com/sparta/able/controller/KeywordController.java
@@ -1,7 +1,11 @@
 package com.sparta.able.controller;
 
+import com.sparta.able.dto.keyword.res.FindKeywordRankResDto;
 import com.sparta.able.service.KeywordService;
+import com.sparta.able.util.ResponseBodyDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +17,9 @@ public class KeywordController {
     private final KeywordService keywordService;
 
     @GetMapping
-    public void findKeywordRank() {
+    public ResponseEntity<ResponseBodyDto<FindKeywordRankResDto>> findKeywordRank() {
+        ResponseBodyDto<FindKeywordRankResDto> responseBody = keywordService.findKeywordRank();
 
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 }
