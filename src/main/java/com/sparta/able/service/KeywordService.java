47c22e0f961d5a76233d4723428fcd97e59e29a7
hemgom <reborn.april23@gmail.com>
2024-11-26 15:07:16 +0900

    [#32] feat: KeywordService.findKeywordRank() 추가

--- src/main/java/com/sparta/able/service/KeywordService.java
+++ src/main/java/com/sparta/able/service/KeywordService.java
@@ -1,11 +1,25 @@
 package com.sparta.able.service;
 
+import com.sparta.able.dto.keyword.res.FindKeywordRankResDto;
+import com.sparta.able.dto.keyword.res.KeywordRankDto;
 import com.sparta.able.repository.KeywordRepository;
+import com.sparta.able.util.ResponseBodyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class KeywordService {
     private final KeywordRepository keywordRepository;
+
+    public ResponseBodyDto<FindKeywordRankResDto> findKeywordRank() {
+        List<KeywordRankDto> foundRanks = keywordRepository.findKeywordRank();
+        FindKeywordRankResDto keywordRanksDto = FindKeywordRankResDto.builder()
+                .keywordRanks(foundRanks)
+                .build();
+
+        return ResponseBodyDto.success("검색어 순위 조회 성공", keywordRanksDto);
+    }
 }
