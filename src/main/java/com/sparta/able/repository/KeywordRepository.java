dc711b70bdefd81ddb973bbb7375b7199866212f
hemgom <reborn.april23@gmail.com>
2024-11-26 14:47:28 +0900

    [#32] feat: KeywordRepository 인터페이스 생성

---
+++ src/main/java/com/sparta/able/repository/KeywordRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.able.repository;
+
+import com.sparta.able.entity.Keyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+}
